0baa9522f0f8a34447a5a4e1023bb74378ef87fe
ldevarajan <elitelokesh@gmail.com>
2021-11-14 19:12:14 +0530

    [Lokesh] remove secrets

--- notification/fcm.go
+++ notification/fcm.go
@@ -18,8 +18,8 @@ func SendGMToClient(c *gin.Context) {
 		})
 		return
 	}
-	payload.ApiKey = "AAAAzQZ7nWw:APA91bHB0h0F6SP4ZQBlRoCkq6we36dG9o_hzSb0D1q8Jl5JGSfnqDD_Xc2X4BlZUsgfX_g8hRLovXDgYpoWQp4Q1v2omLU2OwfCMlKlhSAe3XQ0DFuL1yQItawaT9N2u01ZybldoTTQ"
-	payload.Token = "cLV8bpISTGOX6duO73ss3V:APA91bGR4iTWeDyt0X3cj-FYlXpvRHD_DvdfVeaZ340QX3enl_aga3F-Lr4shvsX2C7R-_-BVbCUmhwNdh7H_d7fm0EMLx9_yrUuxVg7WJn7VZ9o4hTGgsJtQjB7CuX-kDj_HHhQy309"
+	payload.ApiKey = ""
+	payload.Token = ""
 	sendNotification(c, payload)
 }
 
